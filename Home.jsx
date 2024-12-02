98f20d95ed316dcd6db2f79741dafb68240b76d9
Akunna Onyekachi <93290304+akunna1@users.noreply.github.com>
2024-12-02 01:13:30 -0500

    Add files via upload

--- Home.jsx
+++ Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Navbar from "../components/Navbar";
+import Hero from '../components/Hero';
+import Footer from '../components/Footer';
+// First page (home page)
 
-const main = () => {
+const Home = () => {
   return (
     <div>
         <Navbar/>
+        <Hero/>
+        <Footer/>
     </div>
   );
 };
 
-export default main;
+export default Home;
